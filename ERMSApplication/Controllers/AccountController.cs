8b31da159644122c4575b110d59edb9e5ca7a7df
duncanobunge <dancunobunge@gmail.com>
2019-08-22 16:45:42 +0300

    Add project files.

---
+++ ERMSApplication/Controllers/AccountController.cs
@@ -0,0 +1,55 @@
+﻿using ERMSApplication.DBModel;
+using ERMSApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ERMSApplication.Controllers
+{
+    public class AccountController : Controller
+    {
+        EnterpriseResourceMngtDBEntities objERMDBEntities = new EnterpriseResourceMngtDBEntities();
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            UserModel userModel = new UserModel();
+            return View(userModel);
+        }
+
+        [HttpPost]
+        public ActionResult Register(UserModel userModel)
+        {
+            if (ModelState.IsValid)
+            {
+                User objUser = new DBModel.User();
+                objUser.CreatedOn = DateTime.Now;
+                objUser.Firstname = userModel.Firstname;
+                objUser.Lastname = userModel.Lastname;
+                objUser.Email = userModel.Email;
+                objUser.Password = userModel.Password;
+                objERMDBEntities.Users.Add(objUser);
+                objERMDBEntities.SaveChanges();
+                userModel = new UserModel();
+                userModel.SuccessMessage = "User is Successfully Registered!!";
+                return View("Register");
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ ERMSApplication/Models/UserModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ERMSApplication.Models
+{
+    public class UserModel
+    {
+        public int UserId { get; set; }
+        [Required(AllowEmptyStrings = false,ErrorMessage = "First Name is Required.")]
+        [Display(Name = "First Name: ")]
+        public string Firstname { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Last Name is Required.")]
+        [Display(Name = "Last Name: ")]
+        public string Lastname { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email Address is Required.")]
+        [Display(Name = "Email Address: ")]
+        public string Email { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Password is Required.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password: ")]
+        public string Password { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Confirm Password is Required.")]
+        [DataType(DataType.Password)]
+        [Compare("Password",ErrorMessage = "Password and Confirm Password should match")]
+        [Display(Name = "Confirm Password: ")]
+        public string ConfirmPassword { get; set; }
+        [Display(Name = "Date of Creation: ")]
+        public DateTime CreatedOn { get; set; }
+        public string SuccessMessage { get; set; }
+    }
+}
